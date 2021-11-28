44eecd97f2b0961b047e55bbcac74746fcd8d38f
Farhan Almasyhur <farhan.almasyhur@gmail.com>
2021-11-28 21:18:37 +0800

    chore: split into files

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
 # Required imports
 from flask import Flask, request
-from firebase_admin import credentials, initialize_app, db
-
 from flask.json import jsonify
-from models import Laptop
 import pickle
 import numpy as np
+from db import jsonList, laptopList
 
 # Initialize Flask app
 app = Flask(__name__)
 
-# Initialize Firestore DB
-cred = credentials.Certificate('findtechKey.json')
-default_app = initialize_app(cred, {
-    'databaseURL':'https://findtech-d396c-default-rtdb.asia-southeast1.firebasedatabase.app'
-})
-ref = db.reference('/Laptops')
-snapshot = ref.get()
-
-# Throw snapshot into list
-laptopList = []
-for items in snapshot:
-    laptop = Laptop(items['Name'], items['Price'], items['Image'])
-    laptopList.append(laptop)
-jsonList = [e.serialize() for e in laptopList]
 
 # Load ML Model
 model = pickle.load(open('classifier.pkl','rb'))
---
+++ db.py
@@ -0,0 +1,17 @@
+from firebase_admin import credentials, initialize_app, db
+from models import Laptop
+
+# Initialize Realtime DB Firebase
+cred = credentials.Certificate('findtechKey.json')
+default_app = initialize_app(cred, {
+    'databaseURL':'https://findtech-d396c-default-rtdb.asia-southeast1.firebasedatabase.app'
+})
+ref = db.reference('/Laptops')
+snapshot = ref.get()
+
+# Throw snapshot into list
+laptopList = []
+for items in snapshot:
+    laptop = Laptop(items['Name'], items['Price'], items['Image'])
+    laptopList.append(laptop)
+jsonList = [e.serialize() for e in laptopList]
